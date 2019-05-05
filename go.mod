0d3a333ddd6db2236640eda16dd43ce6b035c805
Patrik Potocki <rambo3swe@gmail.com>
2019-05-05 19:40:59 +0200

    first ugly draft of this cli

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/tazer/aws-ec2-ssh
+
+go 1.12
+
+require (
+	github.com/aws/aws-sdk-go v1.19.24
+	github.com/helloyi/go-sshclient v0.0.0-20190128062616-a4cb49c7e961
+	github.com/manifoldco/promptui v0.3.2
+	golang.org/x/crypto v0.0.0-20190426145343-a29dc8fdc734
+)
---
+++ main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/helloyi/go-sshclient"
+	"github.com/manifoldco/promptui"
+	"golang.org/x/crypto/ssh"
+)
+
+var (
+	user   string
+	passwd string
+	prikey string
+)
+
+func main() {
+	servers := []Server{}
+	log.Print("Starting aws-ec2-ssh 🦊")
+
+	flag.StringVar(&user, "user", "username", "The user of login")
+	flag.StringVar(&passwd, "passwd", "yourpasswd", "The passwd of user")
+	flag.StringVar(&prikey, "privatekey", "/.ssh/id_rsa", "The privatekey of user")
+
+	flag.Parse()
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1"),
+	}))
+
+	ec2Svc := ec2.New(sess)
+
+	result, err := ec2Svc.DescribeInstances(&ec2.DescribeInstancesInput{})
+	if err != nil {
+		log.Fatalf("Err: %v", err)
+	}
+
+	for _, reservation := range result.Reservations {
+		for _, instance := range reservation.Instances {
+			name := ""
+			for _, tag := range instance.Tags {
+				if *tag.Key == "Name" {
+					name = *tag.Value
+				}
+			}
+			servers = append(servers, Server{
+				Name:      name,
+				ID:        *instance.InstanceId,
+				PrivateIP: *instance.PrivateIpAddress,
+			})
+		}
+	}
+
+	templates := &promptui.SelectTemplates{
+		Label:    "{{ . }}?",
+		Active:   "\U0001F336 {{ .Name | cyan }} ({{ .ID | red }})",
+		Inactive: "  {{ .Name | cyan }} ({{ .ID | red }})",
+		Selected: "\U0001F336 {{ .Name | red | cyan }}",
+		Details: `
+--------- Servers ----------
+{{ "Name:" | faint }}	{{ .Name }}
+{{ "ID:" | faint }}	{{ .ID }}
+{{ "Private IP:" | faint }}	{{ .PrivateIP }}`,
+	}
+
+	searcher := func(input string, index int) bool {
+		server := servers[index]
+		name := strings.Replace(strings.ToLower(server.Name), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+
+		return strings.Contains(name, input)
+	}
+
+	prompt := promptui.Select{
+		Label:     "Select Server",
+		Items:     servers,
+		Templates: templates,
+		Size:      20,
+		Searcher:  searcher,
+	}
+
+	index, promptResult, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("You choose %q\n what server %s", promptResult, servers[index].Name)
+	selectedServer := servers[index]
+
+	config := &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.AuthMethod{
+			PublicKeyFile(prikey),
+			// ssh.Password(passwd),
+		},
+		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+			return nil
+		},
+	}
+	client, err := sshclient.Dial("tcp", fmt.Sprintf("%s:22", selectedServer.PrivateIP), config)
+	if err != nil {
+		log.Fatalf("Err: %v", err)
+	}
+	defer client.Close()
+
+	// default terminal
+	if err := client.Terminal(nil).Start(); err != nil {
+		log.Fatalf("Err: %v", err)
+	}
+
+	// with a terminal config
+	termConfig := &sshclient.TerminalConfig{
+		Term:   "xterm",
+		Weight: 80,
+		Modes: ssh.TerminalModes{
+			ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+			ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+		},
+	}
+	if err := client.Terminal(termConfig).Start(); err != nil {
+		log.Fatalf("Err: %v", err)
+	}
+}
+
+func PublicKeyFile(file string) ssh.AuthMethod {
+
+	home, err := os.UserHomeDir()
+
+	if err != nil {
+		log.Fatalf("Get home: %s err: %v", file, err)
+	}
+
+	buffer, err := ioutil.ReadFile(home + file)
+	if err != nil {
+		log.Fatalf("Couldnt read home: %s file: %s err: %v", home, file, err)
+	}
+
+	key, err := ssh.ParsePrivateKey(buffer)
+	if err != nil {
+		log.Fatalf("Couldnt ParsePrivateKey file: %s err: %v", file, err)
+	}
+	return ssh.PublicKeys(key)
+}
+
+type Server struct {
+	Name      string
+	ID        string
+	PrivateIP string
+	PublicIP  string
+}
+
+func (srv *Server) String() string {
+	return fmt.Sprintf("%s (%s)", srv.Name, srv.ID)
+}
